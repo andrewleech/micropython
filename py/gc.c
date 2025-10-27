e910846a10df0511aa920d3bb2fbd2c28356b6c9
Andrew Leech <andrew.leech@planetinnovation.com.au>
2025-10-28 07:26:23 +1100

    py/gc: Remove debug fprintf statements from gc_init.
    Clean up temporary debug prints that were used during TLS
    initialization troubleshooting.
    
    Signed-off-by: Andrew Leech <andrew.leech@planetinnovation.com.au>

--- py/gc.c
+++ py/gc.c
@@ -217,17 +217,7 @@ void gc_init(void *start, void *end) {
 
     // unlock the GC
     #if MICROPY_PY_THREAD
-    fprintf(stderr, "[gc_init] About to set gc_lock_depth\n");
-    mp_state_thread_t *tls_check = mp_thread_get_state();
-    fprintf(stderr, "[gc_init] TLS pointer: %p\n", (void *)tls_check);
-    if (tls_check == NULL) {
-        fprintf(stderr, "[gc_init] ERROR: TLS is NULL!\n");
-        exit(1);
-    }
-    #endif
     MP_STATE_THREAD(gc_lock_depth) = 0;
-    #if MICROPY_PY_THREAD
-    fprintf(stderr, "[gc_init] gc_lock_depth set successfully\n");
     #endif
 
     // allow auto collection
